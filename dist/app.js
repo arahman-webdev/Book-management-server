a41a59188a3777c6f0d6b03410e851847541c674
arahman225 <upworkabdurrahman@gmail.com>
2025-07-05 15:32:01 +0600

    added readme file

--- dist/app.js
+++ dist/app.js
@@ -4,9 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const book_router_1 = require("./apps/routes/book.router");
+const borrow_router_1 = require("./apps/routes/borrow.router");
+const cors = require('cors');
 const app = (0, express_1.default)();
+app.use(cors({ origin: ['http://localhost:5173', 'https://abookshelfs.netlify.app'] }));
 app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send("Hi book borrower");
 });
+app.use('/', book_router_1.router);
+app.use('/', borrow_router_1.borrowRouter);
 exports.default = app;
--- dist/apps/routes/book.router.js
+++ dist/apps/routes/book.router.js
@@ -1,2 +1,98 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = __importDefault(require("express"));
+const book_model_1 = require("../models/book.model");
+exports.router = (0, express_1.default)();
+exports.router.post('/api/book', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const newBook = new book_model_1.Book(req.body);
+        const savedBook = yield newBook.save();
+        res.status(200).json({
+            success: true,
+            message: "Book created successfully",
+            data: savedBook
+        });
+    }
+    catch (error) {
+        res.status(400).json({
+            message: "Validation failed",
+            success: false,
+            error
+        });
+    }
+}));
+exports.router.get('/api/books', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const getBook = yield book_model_1.Book.find({});
+    res.status(200).json({
+        success: true,
+        data: getBook
+    });
+}));
+exports.router.get('/api/books/:bookId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const book = req.params.bookId;
+        const getSingleBook = yield book_model_1.Book.findById(book);
+        res.send(getSingleBook);
+        res.status(200).json({
+            success: true,
+            message: "Book retrieved successfully"
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}));
+exports.router.put('/api/books/:bookId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const filter = req.params.bookId;
+        const updateData = req.body;
+        const updatedBook = yield book_model_1.Book.findByIdAndUpdate(filter, updateData, {
+            new: true,
+            runValidators: true,
+        });
+        res.status(200).json({
+            success: true,
+            message: "Book updated successfully",
+            data: updatedBook,
+        });
+    }
+    catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message,
+            error
+        });
+    }
+}));
+// remove a book
+exports.router.delete('/api/books/:bookId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const bookId = req.params.bookId;
+        const deleteBook = yield book_model_1.Book.findByIdAndDelete(bookId);
+        res.status(200).json({
+            success: true,
+            message: "Book deleted successfully",
+            data: deleteBook
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(404).json({
+            success: false,
+            message: "Failed to delete a book",
+        });
+    }
+}));
